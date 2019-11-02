c39f4f2eeb3f7d39deecc969e2e6f5dbee6b1dce
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-02 21:48:08 +0100

    created empty spring boot project

---
+++ kanban-backend-app/src/test/java/com/domko/kanbanbackendapp/KanbanBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domko.kanbanbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
