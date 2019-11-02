ba799249e1d56c9642cea38f7c45e0dc0e3b2e09
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-02 21:48:08 +0100

    created empty spring boot project

---
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/KanbanBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.domko.kanbanbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanBackendAppApplication.class, args);
+	}
+
+}
