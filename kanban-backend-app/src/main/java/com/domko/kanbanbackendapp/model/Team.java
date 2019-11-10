fb2468cf3338e120deca61a33c13ed961a05b68e
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-10 18:48:20 +0100

    maybe working saving ManyToMany relation in user_team table

--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/model/Team.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/model/Team.java
@@ -1,116 +1,25 @@
 package com.domko.kanbanbackendapp.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
 import javax.persistence.*;
-import java.io.Serializable;
-import java.util.*;
+import java.util.Set;
 
-
+@Data
 @Entity
 @Table(name = "team")
-public class Team implements Serializable {
-
-	@OneToMany(mappedBy = "team",
-			cascade = CascadeType.ALL,
-			orphanRemoval = true)
-	private List<UserTeam> users = new ArrayList<>();
-
-//	@JsonIgnore
-//	@ManyToMany(cascade = {CascadeType.ALL})
-//	@JoinTable(
-//			name = "user_team",
-//			joinColumns = {@JoinColumn(name = "team_id")},
-//			inverseJoinColumns = {@JoinColumn(name = "user_id")}
-//	)
-//	Set<User> users = new HashSet<>();
+public class Team {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "team_id")
 	private Long teamId;
+
 	@Column(name = "name")
 	private String name;
 
-	@Override
-	public String toString() {
-		return "Team{" +
-				"users=" + users +
-				", teamId=" + teamId +
-				", name='" + name + '\'' +
-				'}';
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Team team = (Team) o;
-		return Objects.equals(getUsers(), team.getUsers()) &&
-				Objects.equals(getTeamId(), team.getTeamId()) &&
-				Objects.equals(getName(), team.getName());
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(getUsers(), getTeamId(), getName());
-	}
-
-	public List<UserTeam> getUsers() {
-		return users;
-	}
-
-	public void setUsers(List<UserTeam> users) {
-		this.users = users;
-	}
-
-	public Long getTeamId() {
-		return teamId;
-	}
-
-	public void setTeamId(Long teamId) {
-		this.teamId = teamId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Team() {
-	}
-
-	public Team(List<UserTeam> users, Long teamId, String name) {
-		this.users = users;
-		this.teamId = teamId;
-		this.name = name;
-	}
-
-	public void addUser(User user) {
-		UserTeam userTeam = new UserTeam();
-		userTeam.setTeam(this);
-		userTeam.setUser(user);
-
-		users.add(userTeam);
-		user.getTeams().add(userTeam);
-	}
-
-	public void removeUser(User user) {
-		for (Iterator<UserTeam> iterator = users.iterator(); iterator.hasNext(); ) {
-			UserTeam userTeam = iterator.next();
-
-			if (userTeam.getTeam().equals(this) && userTeam.getUser().equals(user)) {
-				iterator.remove();
-				userTeam.getUser().getTeams().remove(userTeam);
-				userTeam.setTeam(null);
-				userTeam.setUser(null);
-			}
-		}
-	}
-
+	@JsonIgnore
+	@OneToMany(mappedBy = "team")
+	private Set<UserTeam> userTeams;
 }
--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/model/UserTeam.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/model/UserTeam.java
@@ -1,100 +1,29 @@
 package com.domko.kanbanbackendapp.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
-@Table(name = "user_team")
-public class UserTeam implements Serializable {
+@Data
+public class UserTeam {
 
 	@EmbeddedId
-	private UserTeamId id;
+	private UserTeamKey id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@MapsId("userId")
-//	@JoinColumn(name = "user_id")
+	@ManyToOne
+	@MapsId("user_id")
+	@JoinColumn(name = "user_id")
 	private User user;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@MapsId("teamId")
-//	@JoinColumn(name = "team_id")
+
+	@ManyToOne
+	@MapsId("team_id")
+	@JoinColumn(name = "team_id")
 	private Team team;
 
 	@Column(name = "team_role")
 	@Enumerated(EnumType.STRING)
-	private TeamRole teamRole = TeamRole.MEMBER;
-
-	@Override
-	public String toString() {
-		return "UserTeam{" +
-				"id=" + id +
-				", user=" + user +
-				", team=" + team +
-				", teamRole=" + teamRole +
-				'}';
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		UserTeam userTeam = (UserTeam) o;
-		return Objects.equals(getId(), userTeam.getId()) &&
-				Objects.equals(getUser(), userTeam.getUser()) &&
-				Objects.equals(getTeam(), userTeam.getTeam()) &&
-				getTeamRole() == userTeam.getTeamRole();
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(getId(), getUser(), getTeam(), getTeamRole());
-	}
-
-	public UserTeamId getId() {
-		return id;
-	}
-
-	public void setId(UserTeamId id) {
-		this.id = id;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public Team getTeam() {
-		return team;
-	}
-
-	public void setTeam(Team team) {
-		this.team = team;
-	}
-
-	public TeamRole getTeamRole() {
-		return teamRole;
-	}
-
-	public void setTeamRole(TeamRole teamRole) {
-		this.teamRole = teamRole;
-	}
-
-	public UserTeam() {
-	}
-
-	public UserTeam(UserTeamId id, User user, Team team, TeamRole teamRole) {
-		this.id = id;
-		this.user = user;
-		this.team = team;
-		this.teamRole = teamRole;
-	}
+	private TeamRole teamRole;
 }
-
