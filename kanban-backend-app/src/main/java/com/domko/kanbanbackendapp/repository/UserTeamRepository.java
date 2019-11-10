78c58303dd17935e9c6ab582fff83668253a0fe6
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-10 18:48:20 +0100

    maybe working saving ManyToMany relation in user_team table

--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/repository/UserTeamRepository.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/repository/UserTeamRepository.java
@@ -1,12 +1,11 @@
 package com.domko.kanbanbackendapp.repository;
 
 import com.domko.kanbanbackendapp.model.UserTeam;
-import com.domko.kanbanbackendapp.model.UserTeamId;
+import com.domko.kanbanbackendapp.model.UserTeamKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserTeamRepository extends JpaRepository<UserTeam, UserTeamId> {
-
+public interface UserTeamRepository extends JpaRepository<UserTeam, UserTeamKey> {
 
 }
