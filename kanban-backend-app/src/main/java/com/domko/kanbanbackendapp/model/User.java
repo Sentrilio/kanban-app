6ac597dcb28915e882b6fcc70d3cee6133b63ff4
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-19 20:18:05 +0100

    working jwt token authorization on backend

--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/model/User.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/model/User.java
@@ -3,15 +3,25 @@ package com.domko.kanbanbackendapp.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.*;
 
-@Data
+
 @Entity
-@Table(name = "user_account")
+@Table(name = "user_account",
+		uniqueConstraints = {
+				@UniqueConstraint(columnNames = "username"),
+				@UniqueConstraint(columnNames = "email")
+		})
+@Data
 public class User implements Serializable {
 
 	@Id
@@ -19,16 +29,22 @@ public class User implements Serializable {
 	@Column(name = "user_id")
 	private Long userId;
 
+	@NotBlank
+	@Size(max = 20)
+	@Column(name = "username")
+	private String username;
+
+	@NotBlank
+	@Size(max = 50)
 	@Column(name = "email")
 	private String email;
 
+	@NotBlank
+	@Size(max = 120)
 	@Column(name = "password_hash")
 	private String passwordHash;
 
-	@Column(name = "nickname")
-	private String nickname;
-
-//	@JsonBackReference
+	//	@JsonBackReference
 	@JsonIgnore
 	@OneToMany(mappedBy = "user")
 	private Set<UserTeam> userTeams;
@@ -37,4 +53,18 @@ public class User implements Serializable {
 	@OneToMany(mappedBy = "user")
 	private Set<UserTask> userTasks;
 
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	public User(String username, String email, String passwordHash) {
+		this.username = username;
+		this.email = email;
+		this.passwordHash = passwordHash;
+	}
+
+	public User() {
+	}
 }
