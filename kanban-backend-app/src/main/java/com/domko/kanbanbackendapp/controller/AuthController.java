917d93946d43944a6842fa2d1b5b3b6654b54b80
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-19 20:18:05 +0100

    working jwt token authorization on backend

---
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/controller/AuthController.java
@@ -0,0 +1,130 @@
+package com.domko.kanbanbackendapp.controller;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import com.domko.kanbanbackendapp.model.ERole;
+import com.domko.kanbanbackendapp.model.Role;
+import com.domko.kanbanbackendapp.model.User;
+import com.domko.kanbanbackendapp.payload.request.LoginRequest;
+import com.domko.kanbanbackendapp.payload.request.SignupRequest;
+import com.domko.kanbanbackendapp.payload.response.JwtResponse;
+import com.domko.kanbanbackendapp.payload.response.MessageResponse;
+import com.domko.kanbanbackendapp.repository.RoleRepository;
+import com.domko.kanbanbackendapp.repository.UserRepository;
+import com.domko.kanbanbackendapp.security.UserDetailsImpl;
+import com.domko.kanbanbackendapp.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Optional<Role> adminRole = roleRepository.findByName(ERole.ROLE_ADMIN);
+//                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        adminRole.ifPresent(roles::add);
+
+                        break;
+                    case "mod":
+                        Optional<Role> modRole = roleRepository.findByName(ERole.ROLE_MODERATOR);
+//                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        modRole.ifPresent(roles::add);
+
+                        break;
+                    default:
+                        Optional<Role> userRole = roleRepository.findByName(ERole.ROLE_USER);
+//                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        userRole.ifPresent(roles::add);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
