9a131bd8eb0fbfe11a184bb220028c481eeb827d
Sentrilio <dominik.kowalski@protonmail.com>
2019-11-19 20:18:05 +0100

    working jwt token authorization on backend

---
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.domko.kanbanbackendapp.security.services;
+
+import com.domko.kanbanbackendapp.model.User;
+import com.domko.kanbanbackendapp.repository.UserRepository;
+import com.domko.kanbanbackendapp.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
