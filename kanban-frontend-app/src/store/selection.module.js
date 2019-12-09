d73ad2b86af16d67b9a56e2324c285179e727c5f
Sentrilio <dominik.kowalski@protonmail.com>
2019-12-09 23:51:04 +0100

    probably working store

--- kanban-frontend-app/src/store/selection.module.js
+++ kanban-frontend-app/src/store/selection.module.js
@@ -1,27 +1,27 @@
-// export const selection = {
-//     namespaced: true,
-//     state: {
-//         selectedTeam: null,
-//         selectedBoard: null,
-//     },
-//     mutations: {
-//         setSelectedTeam(state, selectedTeam) {
-//             console.log("team mutation");
-//             state.selectedTeam = selectedTeam;
-//         },
-//         setSelectedBoard(state, selectedBoard) {
-//             console.log("board mutation");
-//             state.selectedBoard = selectedBoard;
-//         }
-//     },
-//     actions: {
-//         setSelectedTeam({ commit }, selectedTeam) {
-//             console.log("team action");
-//             commit('setSelectedTeam', selectedTeam);
-//         },
-//         setSelectedBoard({ commit }, selectedBoard) {
-//             console.log("board action");
-//             commit('setSelectedBoard', selectedBoard);
-//         }
-//     },
-// };
+export const selection = {
+    namespaced: true,
+    state: {
+        selectedTeamId: null,
+        selectedBoardId: null,
+    },
+    mutations: {
+        setSelectedTeam(state, selectedTeam) {
+            console.log("team mutation");
+            state.selectedTeamId = selectedTeam.teamId;
+        },
+        setSelectedBoard(state, selectedBoard) {
+            console.log("board mutation");
+            state.selectedBoardId = selectedBoard.boardId;
+        }
+    },
+    actions: {
+        setSelectedTeam({ commit }, selectedTeam) {
+            console.log("team action");
+            commit('setSelectedTeam', selectedTeam);
+        },
+        setSelectedBoard({ commit }, selectedBoard) {
+            console.log("board action");
+            commit('setSelectedBoard', selectedBoard);
+        }
+    },
+};
