94e8f47350941964e9af8abc0a4916449087b9d2
Sentrilio <dominik.kowalski@protonmail.com>
2019-12-09 23:00:40 +0100

    some changes in store logic

--- kanban-frontend-app/src/store/index.js
+++ kanban-frontend-app/src/store/index.js
@@ -2,19 +2,19 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import { auth } from './auth.module';
+// import { selection } from './selection.module';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    selectedTeam: null,
-  },
-  mutations: {
-    setSelectedTeam(state, selectedTeam) {
-      state.selectedTeam = selectedTeam;
-    }
-  },
+  strict: true,
+  plugins: [
+    createPersistedState()
+  ],
+
   modules: {
-    auth
+    auth,
+    // selection
   }
 });
