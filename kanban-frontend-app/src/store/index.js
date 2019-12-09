8a72f12391bb0cd924763705bc09de97ac329697
Sentrilio <dominik.kowalski@protonmail.com>
2019-12-09 23:51:04 +0100

    probably working store

--- kanban-frontend-app/src/store/index.js
+++ kanban-frontend-app/src/store/index.js
@@ -2,19 +2,23 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import { auth } from './auth.module';
-// import { selection } from './selection.module';
+import { selection } from './selection.module';
 import createPersistedState from 'vuex-persistedstate';
+// import * as Cookies from 'js-cookie'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  
   strict: true,
   plugins: [
     createPersistedState()
+    // {getState: (key) => Cookies.getJSON(key),
+      // setState: (key, state) => Cookies.set(key, state, { expires: 3, secure: true })}
   ],
 
   modules: {
     auth,
-    // selection
+    selection
   }
 });
