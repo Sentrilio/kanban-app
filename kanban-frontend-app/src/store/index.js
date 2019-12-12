9813874418c97a0bebbcc70d065d2f2535375b18
Sentrilio <dominik.kowalski@protonmail.com>
2019-12-12 17:26:23 +0100

    working board creation in specific team

--- kanban-frontend-app/src/store/index.js
+++ kanban-frontend-app/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import { auth } from './auth.module';
+import { user } from './user.data';
 import { selection } from './selection.module';
 import createPersistedState from 'vuex-persistedstate';
 // import * as Cookies from 'js-cookie'
@@ -9,16 +10,17 @@ import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  
+
   strict: true,
   plugins: [
     createPersistedState()
     // {getState: (key) => Cookies.getJSON(key),
-      // setState: (key, state) => Cookies.set(key, state, { expires: 3, secure: true })}
+    // setState: (key, state) => Cookies.set(key, state, { expires: 3, secure: true })}
   ],
 
   modules: {
     auth,
-    selection
+    selection,
+    user
   }
 });
