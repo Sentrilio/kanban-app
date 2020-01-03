268c53aa78443cde5da8d4366bfae0c6c5d9240a
Sentrilio <dominik.kowalski@protonmail.com>
2020-01-03 19:54:47 +0100

    created TeamMember component

---
+++ kanban-frontend-app/src/components/TeamMember.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a>{{member.user.username}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamMember",
+  props: {
+    member: Object
+  }
+};
+</script>
+<style lang="css" scoped>
+</style>
--- kanban-frontend-app/src/components/VueDraggable.vue
+++ kanban-frontend-app/src/components/VueDraggable.vue
@@ -57,22 +57,6 @@ export default {
       return this.$props.column;
     }
   },
-  data() {
-    return {
-      taskDescription: "",
-      list1: [
-        { name: "John", id: 1 },
-        { name: "Joao", id: 2 },
-        { name: "Jean", id: 3 },
-        { name: "Gerard", id: 4 }
-      ],
-      list2: [
-        { name: "Juan", id: 5 },
-        { name: "Edgard", id: 6 },
-        { name: "Johnson", id: 7 }
-      ]
-    };
-  },
   methods: {
     createTask() {
       TaskService.createTask(this.column.id, this.taskDescription)
