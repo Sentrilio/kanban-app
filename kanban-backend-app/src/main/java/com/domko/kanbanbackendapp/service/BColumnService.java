d6720d798dc0a99b2e0f19ee27f03bf7115d7e0c
Sentrilio <dominik.kowalski@protonmail.com>
2020-01-05 22:46:58 +0100

    changes in services

--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/service/BColumnService.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/service/BColumnService.java
@@ -2,6 +2,10 @@ package com.domko.kanbanbackendapp.service;
 
 import com.domko.kanbanbackendapp.model.BColumn;
 
+import java.util.Optional;
+
 public interface BColumnService {
     BColumn save(BColumn BColumn);
+
+    Optional<BColumn> findBColumn(Long id);
 }
--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/service/TaskService.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/service/TaskService.java
@@ -1,6 +1,8 @@
 package com.domko.kanbanbackendapp.service;
 
+import com.domko.kanbanbackendapp.model.BColumn;
 import com.domko.kanbanbackendapp.model.Task;
+import com.domko.kanbanbackendapp.payload.request.UpdateTaskRequest;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,8 @@ public interface TaskService {
     Optional<Task> findById(Long id);
 
     Task save(Task task);
+
+    void delete(Task task);
+
+    boolean updateTask(Task task, BColumn bColumn, UpdateTaskRequest updateTaskRequest);
 }
