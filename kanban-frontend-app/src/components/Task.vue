2c05853c63776d520fedce1d14c4224f27a238e2
Sentrilio <dominik.kowalski@protonmail.com>
2020-01-07 00:29:36 +0100

    added task click handle (to do: make window prompt when task clicked)

--- kanban-frontend-app/src/components/Task.vue
+++ kanban-frontend-app/src/components/Task.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="list-group-item task">{{task.description}}</div>
+  <div class="list-group-item btn task" @click="logTask(task)">{{task.description}}</div>
 </template>
 <script>
 export default {
   props: {
     task: Object
+  },
+  methods: {
+    logTask(task) {
+      console.log("clicked" + task.id);
+      //to do - add window prompt with task settings
+    }
   }
 };
 </script>
 <style lang="css" scoped>
-/* .task {
-  background-color: white;
-  margin: 5px;
-} */
+.btn {
+  text-align: left;
+}
 </style>
