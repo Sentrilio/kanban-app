2b35bfbf9d2abdc202a36beac8137a52c6f4f309
Sentrilio <dominik.kowalski@protonmail.com>
2020-01-20 17:28:31 +0100

    Refactor Transactional annotation import

--- kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/service/implementation/TaskServiceImpl.java
+++ kanban-backend-app/src/main/java/com/domko/kanbanbackendapp/service/implementation/TaskServiceImpl.java
@@ -13,12 +13,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@org.springframework.transaction.annotation.Transactional
+@Transactional
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
